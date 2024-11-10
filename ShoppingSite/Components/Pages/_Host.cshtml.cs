7ac35077b3dd549f94310d11f9de7ae13037cec1
vosw1 <vosw1@naver.com>
2024-11-10 21:13:08 +0900

    초기 nav 만들기

---
+++ ShoppingSite/Components/Pages/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShoppingSite.Components.Pages
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- ShoppingSite/obj/Debug/net8.0/ShoppingSite.RazorAssemblyInfo.cs
+++ ShoppingSite/obj/Debug/net8.0/ShoppingSite.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingSite")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a8a6ea0db0dbd6701d459c1d8597cc551126c8")]
-[assembly: System.Reflection.AssemblyProductAttribute("ShoppingSite")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ShoppingSite")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
 
